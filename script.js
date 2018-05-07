c9304741cc843ac9498c874f75bb12748f4b49b3
unknown <aaronedelman@yahoo.com>
2018-05-07 07:55:57 -0500

    added skyline slideshow

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Travel Directions
+
 function initMap() {
     var directionsService = new google.maps.DirectionsService;
     var directionsDisplay = new google.maps.DirectionsRenderer;
@@ -103,3 +105,28 @@ $( function() {
     });
     $( "#drive" ).val( $( "#slider-drive" ).slider( "value" ) );
   } );
+
+
+
+//Weather slideshow
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 7000); // Change image every 2 seconds
+}
+
