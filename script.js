fe25b1f13eb5c607558fbc506cdc67d576d50b6b
unknown <aaronedelman@yahoo.com>
2018-05-09 14:45:34 -0500

    alphbatized script.js and styles.css

--- script.js
+++ script.js
@@ -1,192 +1,6 @@
 var weatherObj;
 
-function getCityIds(){
-  var cityIds=[
-  {city:'milwaukee', cityId: 5263045}, //milwaukee
-  {city:'minneapolis', cityId: 4275586}, //minneapolis
-  {city:'dallas', cityId: 4462896}, //dallas
-  {city:'chicago', cityId: 4887398} //chicago
-  ];
-  return cityIds;
-}
-
-function getWeatherAPICallString(){
-  var APIkey = '74bd7bb4aeb708042caefaaaec71be6b';
-  var cityIds = getCityIds();
-  var APIcall = 'http://api.openweathermap.org/data/2.5/group?id=';
-  for(var i=0; i<cityIds.length; i++){
-    if(i < cityIds.length-1){
-    APIcall += cityIds[i].cityId + ',';
-    }
-    else{
-      APIcall += cityIds[i].cityId;
-    }
-  }
-  APIcall += '&APPID=' + APIkey + '&units=imperial';
-  return APIcall
-}
-
-(function getWeather(){
-  var APIcall = getWeatherAPICallString();
-  //console.log(APIcall)
-  fetch(APIcall)
-    .then(
-      function(response){
-        if(response.status !== 200){
-          alert('Weather failed due to error:' + response.status);
-        }
-        else{
-          response.json().then(function(data) {
-          parseWeatherJSON(data);
-          storeWeatherJSON(data);
-        });
-      }
-      });
-})();
-
-function parseWeatherJSON(data){
-  var slides = document.getElementsByClassName("weather");
-  for(var i = 0; i<slides.length; i++)
-  {
-    var slide = slides[i].id;
-    for(var k = 0; k<data.cnt; k++)
-    {
-      if(slide.includes(data.list[k].name.toLowerCase())){
-      document.getElementById(slide).innerHTML = data.list[k].main.temp.toFixed(1) + "&deg"+"F <br>" + data.list[k].weather[0].description;//sets visible text on slide
-      document.getElementById(slide).value = data.list[k];
-      }
-    }
-  }
-}
-
-function storeWeatherJSON(data){
-  weatherObj = data;
-}
-
-
-function initMap() {
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 7,
-        center: {lat: 41.85, lng: -87.65}
-    });
-    var origin;
-    var geocoder = new google.maps.Geocoder();
-    directionsDisplay.setMap(map);
-    directionsDisplay.setPanel(document.getElementById('directionsPanel'));
-    document.getElementById('submit').addEventListener('click', function() {
-        geocodeAddress(directionsService, directionsDisplay, geocoder, calculateAndDisplayRoute);
-    });
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-}
-
-function getDestination(office){
-    var destinationLatLng;
-    switch(office){
-        case 'milwaukee':
-        destinationLatLng = '43.0332535,-87.9061252';
-        break;
-        case 'minneapolis':
-        destinationLatLng = '44.9762798,-93.2687264';
-        break;
-        case 'dallas':
-        destinationLatLng = '32.8090935,-96.8079239';
-        break;
-        case 'chicago':
-        destinationLatLng = '41.8925451,-87.6367606';
-        break;
-    }
-    return destinationLatLng;
-}
-//calculateAndDisplayRoute entered as a callback, as this function would finish before geocoding finishes.
-function geocodeAddress(directionsService, directionsDisplay, geocoder, callback){
-    var address = document.getElementById('address').value;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
-            callback(directionsService, directionsDisplay, results[0].geometry.location);
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-
-}
-function calculateTravelMode(destination){
-  var cityWeatherObj = refineWeatherObj(destination);
-  var bikePoints = Number(document.getElementById('bike').value);
-  var busPoints = Number(document.getElementById('bus').value);
-  var drivePoints = Number(document.getElementById('drive').value);
-  var travelMethod;
-  //temp controls
-  switch(true){
-    case (cityWeatherObj.main.temp < 0):
-    bikePoints -= 100;
-    busPoints -= 20;
-    break;
-    case (cityWeatherObj.main.temp >= 0 && cityWeatherObj.main.temp <= 50):
-    bikePoints -= 30;
-    break;
-    case(cityWeatherObj.main.temp > 50 && cityWeatherObj.main.temp <=85 ):
-    bikePoints += 30;
-    break;
-    case(cityWeatherObj.main.temp >=85):
-    bikePoints -=20;
-    break;
-    default:
-    break;
-  }
-  //element controls
-  var element = cityWeatherObj.weather[0].id.toString();
-  switch(true){
-    case (element.startsWith('2')): //thunderstorm
-    bikePoints -= 80;
-    busPoints -= 10;
-    break;
-    case (element.startsWith('3')): //drizzle
-    bikePoints -=10;
-    break;
-    case (element.startsWith('5')): //rain
-    bikePoints -=30;
-    busPoints -= 20;
-    break;
-    case (element.startsWith('6')): //snow
-    bikePoints -= 100;
-    drivePoints -= 20;
-    busPoints += 20;
-    break;
-    case (element.startsWith('8')): //clear & cloudy
-    bikePoints += 30;
-    break;
-    default: //includes weird weather events
-    break;
-  }
-
-  console.log(bikePoints);
-  console.log(busPoints);
-  console.log(drivePoints);
-  switch(true){
-    case (bikePoints >= busPoints && bikePoints >= drivePoints):
-    return 'BICYCLING';
-    case (busPoints > bikePoints && busPoints >= drivePoints):
-    return 'TRANSIT';
-    case (drivePoints > bikePoints && drivePoints > busPoints):
-    return 'DRIVING';
-    default:
-    return 'DRIVING';
-  }
-  
-}
-
-function refineWeatherObj(destination){
-  console.log(weatherObj);
-  for(var i=0; i<weatherObj.cnt; i++)
-  {
-    if(weatherObj.list[i].name.toLowerCase() === destination){
-      return weatherObj.list[i];
-    }
-  }
-}
-
+// TRAVEL/MAP CARD
 function calculateAndDisplayRoute(directionsService, directionsDisplay, originLatLng) {
     var origin = originLatLng;
     var office = document.getElementById('offices').value;
@@ -206,6 +20,145 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay, originLa
         }
     });
 }
+
+function calculateTravelMode(destination){
+  var cityWeatherObj = refineWeatherObj(destination);
+  var bikePoints = Number(document.getElementById('bike').value);
+  var busPoints = Number(document.getElementById('bus').value);
+  var drivePoints = Number(document.getElementById('drive').value);
+  var travelMethod;
+  //temp controls
+  switch(true){
+    case (cityWeatherObj.main.temp < 0):
+      bikePoints -= 100;
+      busPoints -= 20;
+      break;
+    case (cityWeatherObj.main.temp >= 0 && cityWeatherObj.main.temp <= 50):
+      bikePoints -= 30;
+      break;
+    case(cityWeatherObj.main.temp > 50 && cityWeatherObj.main.temp <=85 ):
+      bikePoints += 30;
+      break;
+    case(cityWeatherObj.main.temp >=85):
+      bikePoints -=20;
+      break;
+    default:
+      break;
+  }
+  //element controls
+  var element = cityWeatherObj.weather[0].id.toString();
+  switch(true){
+    case (element.startsWith('2')): //thunderstorm
+      bikePoints -= 80;
+      busPoints -= 10;
+      break;
+    case (element.startsWith('3')): //drizzle
+      bikePoints -=10;
+      break;
+    case (element.startsWith('5')): //rain
+      bikePoints -=30;
+      busPoints -= 20;
+      break;
+    case (element.startsWith('6')): //snow
+      bikePoints -= 100;
+      drivePoints -= 20;
+      busPoints += 20;
+      break;
+    case (element.startsWith('8')): //clear & cloudy
+      bikePoints += 30;
+      break;
+      default: //includes weird weather events
+      break;
+  }
+  switch(true){
+    case (bikePoints >= busPoints && bikePoints >= drivePoints):
+    return 'BICYCLING';
+    case (busPoints > bikePoints && busPoints >= drivePoints):
+    return 'TRANSIT';
+    case (drivePoints > bikePoints && drivePoints > busPoints):
+    return 'DRIVING';
+    default:
+    return 'DRIVING';
+  } 
+}
+
+function geocodeAddress(directionsService, directionsDisplay, geocoder, callback){
+    var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            callback(directionsService, directionsDisplay, results[0].geometry.location);
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+}
+
+function getCityIds(){
+  var cityIds=[
+  {city:'milwaukee', cityId: 5263045}, //milwaukee
+  {city:'minneapolis', cityId: 4275586}, //minneapolis
+  {city:'dallas', cityId: 4462896}, //dallas
+  {city:'chicago', cityId: 4887398} //chicago
+  ];
+  return cityIds;
+}
+
+function getDestination(office){
+    var destinationLatLng;
+    switch(office){
+        case 'milwaukee':
+          destinationLatLng = '43.0332535,-87.9061252';
+          break;
+        case 'minneapolis':
+          destinationLatLng = '44.9762798,-93.2687264';
+          break;
+        case 'dallas':
+          destinationLatLng = '32.8090935,-96.8079239';
+          break;
+        case 'chicago':
+          destinationLatLng = '41.8925451,-87.6367606';
+          break;
+    }
+    return destinationLatLng;
+}
+
+function initMap() {
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7,
+        center: {lat: 41.85, lng: -87.65}
+    });
+    var origin;
+    var geocoder = new google.maps.Geocoder();
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('directionsPanel'));
+    document.getElementById('submit').addEventListener('click', function() {
+        geocodeAddress(directionsService, directionsDisplay, geocoder, calculateAndDisplayRoute);
+    });
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+}
+
+function refineWeatherObj(destination){
+  console.log(weatherObj);
+  for(var i=0; i<weatherObj.cnt; i++)
+  {
+    if(weatherObj.list[i].name.toLowerCase() === destination){
+      return weatherObj.list[i];
+    }
+  }
+}
+
+function storeTravelPreferences(){
+  localStorage.setItem("bikePoints", document.getElementById('bike').value);
+  localStorage.setItem("busPoints", document.getElementById('bus').value);
+  localStorage.setItem("drivePoints", document.getElementById('drive').value);
+}
+
+function storeWeatherJSON(data){
+  weatherObj = data;
+}
+
 //travel preference sliders
 $( function() {
     $( "#slider-bike" ).slider({
@@ -249,10 +202,57 @@ $( function() {
     $( "#drive" ).val( $( "#slider-drive" ).slider( "value" ) );
   } );
 
-//Weather slideshow
+
+//WEATHER SLIDESHOW CARD
+(function getWeather(){
+  var APIcall = getWeatherAPICallString();
+  fetch(APIcall)
+    .then(
+      function(response){
+        if(response.status !== 200){
+          alert('Weather failed due to error:' + response.status);
+        }
+        else{
+          response.json().then(function(data) {
+          parseWeatherJSON(data);
+          storeWeatherJSON(data);
+        });
+      }
+      });
+})();
+
+function getWeatherAPICallString(){
+  var APIkey = '74bd7bb4aeb708042caefaaaec71be6b';
+  var cityIds = getCityIds();
+  var APIcall = 'http://api.openweathermap.org/data/2.5/group?id=';
+  for(var i=0; i<cityIds.length; i++){
+    if(i < cityIds.length-1){
+    APIcall += cityIds[i].cityId + ',';
+    }
+    else{
+      APIcall += cityIds[i].cityId;
+    }
+  }
+  APIcall += '&APPID=' + APIkey + '&units=imperial';
+  return APIcall
+}
+
+function parseWeatherJSON(data){
+  var slides = document.getElementsByClassName("weather");
+  for(var i = 0; i<slides.length; i++)
+  {
+    var slide = slides[i].id;
+    for(var k = 0; k<data.cnt; k++)
+    {
+      if(slide.includes(data.list[k].name.toLowerCase())){
+      document.getElementById(slide).innerHTML = data.list[k].main.temp.toFixed(1) + "&deg"+"F <br>" + data.list[k].weather[0].description;//sets visible text on slide
+      document.getElementById(slide).value = data.list[k];
+      }
+    }
+  }
+}
 
 var slideIndex = 0;
-
 (function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
@@ -270,16 +270,8 @@ var slideIndex = 0;
     setTimeout(showSlides, 5000); // Change image every 5 seconds
 })();
 
-
 function test(){
-
 var value = localStorage.getItem("bikePoints");
 console.log(value);
-}
-
-function storeTravelPreferences(){
-  localStorage.setItem("bikePoints", document.getElementById('bike').value);
-  localStorage.setItem("busPoints", document.getElementById('bus').value);
-  localStorage.setItem("drivePoints", document.getElementById('drive').value);
 }
 
